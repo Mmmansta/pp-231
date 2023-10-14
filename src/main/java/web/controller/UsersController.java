8e503b37afa27defa043bbbf32d37ef45e3889c1
Mmmansta <128576312+Mmmansta@users.noreply.github.com>
2023-10-14 18:34:55 +0300

    исправил часть

--- src/main/java/web/controller/UsersController.java
+++ src/main/java/web/controller/UsersController.java
@@ -3,6 +3,7 @@ package web.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -45,7 +46,10 @@ public class UsersController {
     }
 
     @PostMapping("")
-    public String create(@ModelAttribute("user") @Valid User user ) {
+    public String create(@ModelAttribute("user") @Valid User user,BindingResult bindingResult ) {
+        if (bindingResult.hasErrors()){
+           return "new";
+        }
         userService.addUser(user);
         return "redirect:/users";
     }
